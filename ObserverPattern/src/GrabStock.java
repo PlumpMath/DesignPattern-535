d5acea6f310a61ddf10f431b4e0deddbdbc39eb6
uniquews <wstjpu@gmail.com>
2014-07-10 12:11:35 -0700

    add

---
+++ ObserverPattern/src/GrabStock.java
@@ -0,0 +1,16 @@
+
+public class GrabStock {
+
+	public static void main(String[] args) {
+
+		StockGrabber stockGrabber = new StockGrabber();
+		
+		StockObserver observer1 = new StockObserver(stockGrabber);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAAPLPrice(677.60);
+		stockGrabber.setGOOPrice(676.40);
+	}
+
+}
+ 
---
+++ ObserverPattern/src/StockObserver.java
@@ -0,0 +1,34 @@
+
+public class StockObserver implements Observer {
+
+	
+	private double ibmPrice;
+	private double applPrice;
+	private double gooPrice;
+	
+	private static int observerIDTracker = 0;
+	
+	private int observerID;
+	
+	private Subject stockGrabber;
+	
+	public StockObserver(Subject stockGrabber){
+		this.stockGrabber = stockGrabber;
+		this.observerID = ++ observerIDTracker;
+		System.out.println("New Observer "+ this.observerID);
+		stockGrabber.register(this);
+	}
+	
+	@Override
+	public void update(double ibmPrice, double aaplPrice, double gooPrice) {
+		this.applPrice = applPrice;
+		this.gooPrice = gooPrice;
+		this.ibmPrice = ibmPrice;
+		
+		printThePrices();
+	}
+
+	public void printThePrices(){
+		System.out.println(observerID + "\nIBM: "+ ibmPrice + "\nAAPL: "+applPrice + "\nGOOG: "+gooPrice +"\n");
+	}
+}
